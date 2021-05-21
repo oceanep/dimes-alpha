b061efa65514b385dc8bbfcdf581c27d80ecd073
maacarroll <mcarroll@styleseat.com>
2021-05-21 12:19:24 +0900

    update api server endpoint to prod

--- src/utils/user_api.js
+++ src/utils/user_api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-let api_endpoint = 'http://ec2-3-113-15-18.ap-northeast-1.compute.amazonaws.com/api/users'
+let api_endpoint = 'http://ec2-35-74-184-115.ap-northeast-1.compute.amazonaws.com/api/users'
 let headers = {
     "Content-type": "application/json",
     "Accept" : "application/json"
