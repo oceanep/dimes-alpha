f89bba69f560afa3b1a0e3ccb1e5824373caa9e0
kabirvirji <>
2021-05-28 14:37:44 -0700

    added logout functionality

--- src/components/LandingNav/LandingNav.js
+++ src/components/LandingNav/LandingNav.js
@@ -5,14 +5,12 @@ import {
   Spacer,
   Image,
 } from '@chakra-ui/react';
-import {
-  Link
-} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useTranslation } from 'react-i18next'
 
 function LandingNav() {
 
-  const { t, i18n } = useTranslation()
+  const { t } = useTranslation()
 
   return (
     <nav>
@@ -21,13 +19,7 @@ function LandingNav() {
           <Image maxW="100px" src="https://dimes-frontend-assets.s3-ap-northeast-1.amazonaws.com/img/dimes_logo_2.png" alt="Dimes Logo"/>
         </Box>
         <Spacer/>
-        <Link to='/signup'>
-          <Button colorScheme="teal" size="md" ml="15px">{`${t('header.register')}`}</Button>
-        </Link>
-        <Link to='/login'>
-          <Button colorScheme="teal" size="md" ml="15px">{`${t('header.signin')}`}</Button>
-        </Link>
-        <Button colorScheme="teal" size="md" ml="15px">{`${t('header.logout')}`}</Button>
+        {localStorage.getItem("token") != null ? <div><Link to='/landing'><Button onClick={() => localStorage.clear()} colorScheme="teal" size="md" ml="15px">{`${t('header.logout')}`}</Button></Link></div> : <div><Link to='/signup'><Button colorScheme="teal" size="md" ml="15px">{`${t('header.register')}`}</Button></Link><Link to='/login'><Button colorScheme="teal" size="md" ml="15px">{`${t('header.signin')}`}</Button></Link></div>}
       </Flex>
     </nav>
   );
