ce8ab58fd72c14e69a285385acbe8e3daeb03969
maacarroll <mcarroll@styleseat.com>
2021-06-16 14:42:16 +0900

    add gcal access

--- src/components/GoogleApis/GoogleLoginComponent.jsx
+++ src/components/GoogleApis/GoogleLoginComponent.jsx
@@ -7,15 +7,17 @@ const responseGoogle = (response) => {
     console.log(response);
 }
 
-
 const GoogleLoginComponent = ({ props }) => {
     ReactDOM.render(
         <GoogleLogin
-            clientId="330818651692-jfn8mgdaemvds7bnt7qc906r48ebbd71.apps.googleusercontent.com"
+            clientId="330818651692-locc5gad48668ij33bdaptre2mi9irps.apps.googleusercontent.com"
             buttonText="Login"
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
+            approvalPrompt="force"
+            prompt='consent'
             cookiePolicy={'single_host_origin'}
+            scope={"https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid"}
         />,
         document.getElementById('googleButton')
     )
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -187,14 +187,17 @@ function Signup() {
                 )}
             </Formik>
                     ReactDOM.render(
-                     <GoogleLogin
-                     clientId="330818651692-jfn8mgdaemvds7bnt7qc906r48ebbd71.apps.googleusercontent.com"
-                     buttonText="Login with Google"
-                     onSuccess={responseGoogle}
-                     onFailure={responseGoogle}
-                     cookiePolicy={'single_host_origin'}
-                     />,
-                     document.getElementById('googleButton');
+            <GoogleLogin
+              clientId="330818651692-locc5gad48668ij33bdaptre2mi9irps.apps.googleusercontent.com"
+              buttonText="Login"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              approvalPrompt="force"
+              prompt='consent'
+              cookiePolicy={'single_host_origin'}
+              scope={"https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile openid"}
+            />,              
+            document.getElementById('googleButton');
             )
            
           </VStack>
