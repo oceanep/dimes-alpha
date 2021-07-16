426daac2cd10f90153bf4a2cff243345fe0efb43
maacarroll <mcarroll@styleseat.com>
2021-07-16 13:00:05 +0900

    Edit

--- public/google_auth.js
+++ public/google_auth.js
@@ -53,14 +53,15 @@ function initClient() {
 
 function updateSigninStatus(isSignedIn) {
     if (isSignedIn) {
-        authorizeButton.style.display = 'none';
+        //authorizeButton.style.display = 'none';
         //want this to be a chain
-        signoutButton.style.display = 'block';
+        //signoutButton.style.display = 'block';
         listUpcomingEvents();
         listConnectionNames();
     } else {
-        authorizeButton.style.display = 'block';
-        signoutButton.style.display = 'none';
+        console.log("logged in");
+        //authorizeButton.style.display = 'block';
+        //signoutButton.style.display = 'none';
     }
 }
 
